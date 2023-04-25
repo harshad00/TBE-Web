738f46227befce27e850703af5dfc62fab7e552a
Sachin <sachinkshuklaoo7@gmail.com>
2023-04-25 19:30:57 +0530

    Fixed Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 import { PageLayout } from '@/components';
 import '@/styles/globals.css';
 import '@/styles/colors.css';
+import { googleAnalyticsScript, gtag } from '@/constant';
 
 const TheBoringEducation = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <Script async src={gtag}></Script>
+      <Script id='google-analytics'>{googleAnalyticsScript}</Script>
       <PageLayout>
         <Component {...pageProps} />
       </PageLayout>
