b1c389891b252e82c0bf104b7e02b6552fcdeb4f
maddy404 <97498608+maddy404@users.noreply.github.com>
2023-05-03 21:50:40 +0530

    add: dynamic data

--- src/components/containers/Page/Webinar/AboutWebinarInstructorContainer.tsx
+++ src/components/containers/Page/Webinar/AboutWebinarInstructorContainer.tsx
@@ -6,25 +6,33 @@ import {
   WebinarInstructor,
 } from '@/components';
 
+import { WebinarInstructorCardProps } from '@/interfaces'
 const aboutInstructor = [
   '1. Built Ed-tech startups since college.',
   '2. Worked with Newton School, Masai, Pesto & CueMath.',
   '3. Senior Software Engineer @PW.',
 ];
 
-const AboutWebinarInstructorContainer = () => {
+const AboutWebinarInstructorContainer = ({ id,
+  heading,
+  image,
+  imageAltText,
+  name,
+  socialIcon,
+  designation,
+  instructorDetails, }: WebinarInstructorCardProps) => {
   return (
     <Section className='py-6 md:py-8'>
-      <FlexContainer direction='col' className='m-auto w-3/4 gap-5 lg:w-1/3'>
+      <FlexContainer key={id} direction='col' className='m-auto w-3/4 gap-5 lg:w-1/3'>
         <Text level='h4' className='heading-4'>
-          Meet your instructor
+          {heading}
         </Text>
         <FlexContainer direction='col' className='gap-3'>
           <WebinarInstructor
-            imagePath='/images/sachin_shukla.png'
-            imageAltText='Co-founder Sachin Shukla'
-            name='Sachin Kr. Shukla'
-            position='Co-founder The Boring Education'
+            imagePath={image}
+            imageAltText={imageAltText}
+            name={name}
+            position={designation}
             linkedInURL='#'
             containerClasses='gap-2'
           />
@@ -34,7 +42,7 @@ const AboutWebinarInstructorContainer = () => {
               className: 'mt-2',
               itemCenter: false,
             }}
-            paragraphs={aboutInstructor}
+            paragraphs={instructorDetails}
           />
         </FlexContainer>
       </FlexContainer>
