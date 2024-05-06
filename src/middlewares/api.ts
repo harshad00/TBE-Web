fc0737791ef537f64eb81962afc305f94734dc75
Sachin <sachinkshuklaoo7@gmail.com>
2024-05-06 10:10:58 +0530

    Remove Build Log

--- src/middlewares/api.ts
+++ src/middlewares/api.ts
@@ -9,9 +9,13 @@ const connectDB = async (res: NextApiResponse) => {
     await mongoose.connect(MONGODB_URI);
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
-    return res
-      .status(apiStatusCodes.INTERNAL_SERVER_ERROR)
-      .json(sendAPIResponse({ status: false, message: 'DB error', error }));
+    return res.status(apiStatusCodes.INTERNAL_SERVER_ERROR).json(
+      sendAPIResponse({
+        status: false,
+        message: `DB error by passing ${MONGODB_URI}`,
+        error,
+      })
+    );
   }
 };
 
