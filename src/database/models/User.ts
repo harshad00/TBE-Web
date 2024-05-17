37e664769f8469e27ecf119e2720cead08a6265b
krish36pandey@gmail.com <krish36pandey@gmail.com>
2024-05-17 12:31:24 +0530

    added _id to user model

--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -2,28 +2,25 @@ import { databaseModels } from '@/constant';
 import { IUser } from '@/interfaces';
 import { Model, Schema, model, models } from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
-const UserSchema: Schema<IUser> = new Schema(
-  {
-    userId: {
-      type: String,
-      default: uuidv4,
-      unique: true,
-    },
-    name: {
-      type: String,
-      required: [true, 'name is required'],
-    },
-    username: {
-      type: String,
-    },
-    email: {
-      type: String,
-      required: [true, 'email is required'],
-      unique: true,
-    },
+const UserSchema: Schema<IUser> = new Schema({
+  userId: {
+    type: String,
+    default: uuidv4,
+    unique: true,
   },
-  { _id: false }
-);
+  name: {
+    type: String,
+    required: [true, 'name is required'],
+  },
+  username: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, 'email is required'],
+    unique: true,
+  },
+});
 
 const User: Model<IUser> =
   models?.User || model<IUser>(databaseModels.USER, UserSchema);
