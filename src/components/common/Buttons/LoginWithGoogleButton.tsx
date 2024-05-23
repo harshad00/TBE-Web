527d78ef50dcec1c8dc0160f6a695f6e7c0d3fa0
Sachin <sachinkshuklaoo7@gmail.com>
2024-05-23 10:33:14 +0530

    fix minor changes

--- src/components/common/Buttons/LoginWithGoogleButton.tsx
+++ src/components/common/Buttons/LoginWithGoogleButton.tsx
@@ -1,16 +1,16 @@
 import { signIn } from 'next-auth/react';
-
 import Button from './Button';
-
 import { useSession } from 'next-auth/react';
 
 const LoginWithGoogleButton = () => {
   const session = useSession();
-  if (session.status === 'authenticated') return;
+  if (session.status === 'authenticated' || session.status === 'loading')
+    return;
+
   return (
     <Button
       variant='PRIMARY'
-      text='Continue With Google'
+      text='Login'
       onClick={() => {
         signIn('google');
       }}
