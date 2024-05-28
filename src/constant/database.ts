cb26ba45e898676f26129b487a85101abe243fa9
krish36pandey@gmail.com <krish36pandey@gmail.com>
2024-05-28 16:58:08 +0530

    added Course,CourseSection,CourseChapter models

--- src/constant/database.ts
+++ src/constant/database.ts
@@ -1,6 +1,9 @@
 const databaseModels = {
   PROJECT: 'Project',
   USER: 'User',
+  Course: 'Course',
+  COURSE_SECTION: 'CourseSection',
+  COURSE_CHAPTER: 'CourseChapter',
 };
 
 export { databaseModels };
