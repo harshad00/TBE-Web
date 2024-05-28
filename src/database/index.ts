6a4d0409efe1db71679baf232b4b65d76fc76053
krish36pandey@gmail.com <krish36pandey@gmail.com>
2024-05-28 16:59:29 +0530

    added Course,CourseSection,CourseChapter models

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,11 @@
 // Models
 import Project from './models/Project';
 import User from './models/User';
+import Course from './models/Course';
+import CourseChapter from './models/CourseChapter';
+import CourseSection from './models/CourseSection';
 
-export { Project, User };
+export { Project, User, Course, CourseChapter, CourseSection };
 
 // Query
 export * from './query/project';
