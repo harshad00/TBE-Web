a9a5a7ceb6ce2629fd420b158f47c9bbeab8055b
krish36pandey@gmail.com <krish36pandey@gmail.com>
2024-05-29 14:40:20 +0530

    added update & delete section

--- src/database/query/section.ts
+++ src/database/query/section.ts
@@ -1,8 +1,10 @@
 import {
   AddSectionToACourseDBRequestProps,
   DatabaseQueryResponseType,
+  UpdateCourseSectionInDBRequestProps,
 } from '@/interfaces';
-import { CourseSection } from '@/database';
+import { CourseChapter, CourseSection } from '@/database';
+import mongoose from 'mongoose';
 
 const AddSectionToACourseInDB = async (
   sectionData: AddSectionToACourseDBRequestProps
@@ -15,4 +17,67 @@ const AddSectionToACourseInDB = async (
   }
 };
 
-export { AddSectionToACourseInDB };
+const UpdateCourseSectionInDB = async ({
+  updatedData,
+  sectionId,
+}: UpdateCourseSectionInDBRequestProps): Promise<DatabaseQueryResponseType> => {
+  try {
+    const section = await CourseSection.findByIdAndUpdate(
+      sectionId,
+      updatedData,
+      { new: true }
+    );
+    return { data: section };
+  } catch (error) {
+    return { error: "Failed while updating course's section" };
+  }
+};
+
+const DeleteCourseSectionByIdFromDB = async (
+  sectionId: string
+): Promise<DatabaseQueryResponseType> => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    await CourseChapter.deleteMany({ sectionId });
+    await CourseSection.findByIdAndDelete(sectionId);
+    await session.commitTransaction();
+    return { data: 'course deleted' };
+  } catch (error) {
+    await session.abortTransaction();
+    return { error: "Failed while deleting course's section" };
+  }
+};
+
+const GetChapterAssociatedWithSectionByIdFromDB = async (
+  sectionId: string
+): Promise<DatabaseQueryResponseType> => {
+  try {
+    const sectionObjectId = new mongoose.Types.ObjectId(sectionId);
+    const course = await CourseSection.aggregate([
+      {
+        $match: {
+          _id: sectionObjectId,
+        },
+      },
+      {
+        $lookup: {
+          from: 'coursechapters', // collection name of CourseChapter
+          localField: '_id',
+          foreignField: 'sectionId',
+          as: 'chapters',
+        },
+      },
+    ]);
+    return { data: course };
+  } catch (error) {
+    return { error: 'Failed while fetching a section' };
+  }
+};
+
+export {
+  AddSectionToACourseInDB,
+  UpdateCourseSectionInDB,
+  DeleteCourseSectionByIdFromDB,
+  GetChapterAssociatedWithSectionByIdFromDB,
+};
