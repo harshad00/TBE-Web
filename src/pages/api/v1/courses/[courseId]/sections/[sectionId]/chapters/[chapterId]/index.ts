089e2172f9e09c8a6b30906edfe9d55b1f479d61
krish36pandey@gmail.com <krish36pandey@gmail.com>
2024-05-29 14:48:47 +0530

    added endpoint for chapters

---
+++ src/pages/api/v1/courses/[courseId]/sections/[sectionId]/chapters/[chapterId]/index.ts
@@ -0,0 +1,98 @@
+import { apiStatusCodes } from '@/constant';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { sendAPIResponse } from '@/utils';
+import { connectDB } from '@/middlewares';
+import { AddCourseChapterInDBRequestProps } from '@/interfaces';
+import {
+  DeleteCourseChapterByIdFromDB,
+  UpdateCourseChapterInDB,
+} from '@/database/query/chapter';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await connectDB(res);
+  const { method, query } = req;
+  const { chapterId } = query as { chapterId: string };
+
+  switch (method) {
+    case 'POST':
+      return handleUpdateChapter(req, res, chapterId);
+
+    case 'DELETE':
+      return handleDeleteChapter(req, res, chapterId);
+
+    default:
+      return res.status(apiStatusCodes.BAD_REQUEST).json(
+        sendAPIResponse({
+          status: false,
+          message: `Method ${req.method} Not Allowed`,
+        })
+      );
+  }
+};
+
+const handleUpdateChapter = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  chapterId: string
+) => {
+  const updatedData = req.body as Partial<AddCourseChapterInDBRequestProps>;
+
+  try {
+    const { data, error } = await UpdateCourseChapterInDB({
+      updatedData,
+      chapterId,
+    });
+
+    if (error) {
+      return res.status(apiStatusCodes.INTERNAL_SERVER_ERROR).json(
+        sendAPIResponse({
+          status: false,
+          message: 'Failed while updating chapter to section',
+        })
+      );
+    }
+
+    return res
+      .status(apiStatusCodes.OKAY)
+      .json(sendAPIResponse({ status: true, data }));
+  } catch (error) {
+    return res.status(apiStatusCodes.INTERNAL_SERVER_ERROR).json(
+      sendAPIResponse({
+        status: false,
+        message: 'Failed while updating chapter to section',
+      })
+    );
+  }
+};
+
+const handleDeleteChapter = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  chapterId: string
+) => {
+  try {
+    const { data, error } = await DeleteCourseChapterByIdFromDB(chapterId);
+
+    if (error) {
+      return res.status(apiStatusCodes.INTERNAL_SERVER_ERROR).json(
+        sendAPIResponse({
+          status: false,
+          message: 'Failed while deleting chapter to section',
+        })
+      );
+    }
+
+    return res
+      .status(apiStatusCodes.OKAY)
+      .json(sendAPIResponse({ status: true, data }));
+  } catch (error) {
+    return res.status(apiStatusCodes.INTERNAL_SERVER_ERROR).json(
+      sendAPIResponse({
+        status: false,
+        message: 'Failed while deleting chapter to section',
+      })
+    );
+  }
+};
+
+export default handler;
