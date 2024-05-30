a93a84cb261eef2be33d63a53494786b9c786d62
krish36pandey@gmail.com <krish36pandey@gmail.com>
2024-05-30 16:24:23 +0530

    configure middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,11 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { routes } from './constant';
 
+const courseEnrollPathExp = /^\/api\/v1\/courses\/[^/]+\/enroll$/;
+
 const middleware = async (req: NextRequest) => {
   const response = await fetch(
-    `${process.env.BASE_API_URL}/users/isauthenticed`,
+    `${process.env.BASE_API_URL}/users/isauthenticated`,
     {
       credentials: 'include',
       cache: 'no-cache',
@@ -17,15 +19,24 @@ const middleware = async (req: NextRequest) => {
   // if the response is ok that means user is authenticated otherwise unauthenticated
   const currentUrl = req.nextUrl.pathname;
   if (response.ok) {
+    const { data }: { status: boolean; data: { email: string; _id: string } } =
+      await response.json();
+    const { _id } = data;
     if (currentUrl.startsWith(routes.register)) {
       return NextResponse.redirect(new URL('/', req.url));
+    }
+    // "matcing /api/v1/courses/[dynamic]/enroll"
+    else if (courseEnrollPathExp.test(req.nextUrl.pathname)) {
+      const nextResponse = NextResponse.next();
+      nextResponse.headers.set('x-user-id', _id);
+      return nextResponse;
     }
     return NextResponse.redirect(new URL('/', req.url));
   }
 };
 
 export const config = {
-  matcher: '/register',
+  matcher: ['/register', '/api/v1/courses/:courseId/enroll'],
 };
 
 export { middleware };
