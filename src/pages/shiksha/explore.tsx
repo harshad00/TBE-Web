58b49169abcb2320925b66b626aa14a4f8e97957
krish36pandey@gmail.com <krish36pandey@gmail.com>
2024-06-03 16:17:44 +0530

    added explore page

---
+++ src/pages/shiksha/explore.tsx
@@ -0,0 +1,41 @@
+import React, { Fragment } from 'react';
+import { PageProps } from '@/interfaces';
+import { SEO, CardContainerB, LoadingSpinner } from '@/components';
+import { useAPIResponseMapper, useApi } from '@/hooks';
+import {
+  getPreFetchProps,
+  mapCourseResponseToCard,
+  sendRequest,
+} from '@/utils';
+import { routes } from '@/constant';
+
+const Home = ({ seoMeta }: PageProps) => {
+  const { response, loading } = useApi('courses', sendRequest, {
+    url: routes.api.courses,
+  });
+
+  const courses = useAPIResponseMapper(response?.data, mapCourseResponseToCard);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+  console.log(courses);
+
+  return (
+    <Fragment>
+      <SEO seoMeta={seoMeta} />
+      <CardContainerB
+        heading='Explore'
+        focusText='Courses'
+        cards={courses}
+        borderColour={2}
+        subtext='Pick A Real Life Project and Start Building'
+        sectionClassName='px-2 py-4'
+      />
+    </Fragment>
+  );
+};
+
+export const getServerSideProps = getPreFetchProps;
+
+export default Home;
