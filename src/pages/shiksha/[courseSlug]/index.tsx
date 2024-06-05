eb7add4e16a8d468459c04e0abffbd0277e27910
krish36pandey@gmail.com <krish36pandey@gmail.com>
2024-06-05 15:42:12 +0530

    added course page

--- src/pages/shiksha/[courseSlug]/index.tsx
+++ src/pages/shiksha/[courseSlug]/index.tsx
@@ -1,17 +1,75 @@
-import { PrimaryCard } from '@/components';
+import {
+  Accordion,
+  AccordionLinkItem,
+  FlexContainer,
+  MDXRenderer,
+  PageHeroMetaContainer,
+  Section,
+  Text,
+} from '@/components';
+import CourseHeroContainer from '@/components/containers/Page/Course/CourseHeroContainer';
+import { CourseChapterModel } from '@/interfaces';
+import { useState } from 'react';
 
-const Home = ({ course }: any) => {
-  console.log(course);
+const Home = ({ course, courseSlug }: any) => {
+  const [courseMeta, setCourseMeta] = useState<string>(course.meta || '');
+  const handleChapterClick = (chapterMeta: string) => {
+    setCourseMeta(chapterMeta);
+  };
 
   return (
-    <>
-      <PrimaryCard
-        image='/images/frontend_camp_instructor_dp.png'
-        imageAltText='js'
-        title='Free Mentorship'
-        content='"njk'
-      />
-    </>
+    <div className='p-2'>
+      <PageHeroMetaContainer subtitle="YOU'RE LEARING" title={course.title} />
+      <Section className='md:p-2 p-2'>
+        <CourseHeroContainer name={course.title} roadmap={course.roadmap} />
+      </Section>
+      <Section className='md:p-2 p-2'>
+        <FlexContainer className='w-full gap-4' itemCenter={false}>
+          <FlexContainer
+            className='border md:w-3/12 w-full p-2 gap-1 rounded self-baseline'
+            itemCenter={false}
+            direction='col'
+          >
+            <Text level='h5' className='heading-5'>
+              Sections
+            </Text>
+            <FlexContainer justifyCenter={false} className='gap-px'>
+              {course.sections.map(
+                (section: {
+                  _id: string;
+                  title: string;
+                  chapters: Partial<CourseChapterModel>[];
+                }) => {
+                  return (
+                    <Accordion title={section.title} key={section._id}>
+                      {section.chapters.map((chapter) => {
+                        return (
+                          <AccordionLinkItem
+                            key={chapter._id}
+                            label={chapter.title || ''}
+                            href={`${courseSlug}?courseId=${course._id}&sectionId=${section._id}&chapterId=${chapter._id}`}
+                            onClick={() => {
+                              handleChapterClick(chapter.content || '');
+                            }}
+                          />
+                        );
+                      })}
+                    </Accordion>
+                  );
+                }
+              )}
+            </FlexContainer>
+          </FlexContainer>
+          <FlexContainer
+            className='border md:w-8/12 w-full p-2 rounded'
+            justifyCenter={false}
+            itemCenter={false}
+          >
+            <MDXRenderer mdxSource={courseMeta} />
+          </FlexContainer>
+        </FlexContainer>
+      </Section>
+    </div>
   );
 };
 
@@ -27,12 +85,11 @@ export const getServerSideProps = async ({
     );
     const data = await response.json();
 
-    if (!data.status || !response.ok) return { notFound: true };
+    if (!data.status || !response.ok || !data.data || data.data.length === 0)
+      return { notFound: true };
 
-    return { props: { course: data.data } };
+    return { props: { course: data.data[0], courseSlug: query.courseSlug } };
   } catch (error) {
-    console.log(error);
-
     return { notFound: true };
   }
 };
