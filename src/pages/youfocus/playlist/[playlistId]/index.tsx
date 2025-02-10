6c511639cefb332ff31cc1419291a99c428e1c4c
Harshad Prajapati <bholo0090@gmail.com>
2025-02-10 07:09:32 +0530

    feat: update PlaylistPage to use PlaylistCantainerCard component and adjust props structure

--- src/pages/youfocus/playlist/[playlistId]/index.tsx
+++ src/pages/youfocus/playlist/[playlistId]/index.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { PageProps } from "@/interfaces";
-import { SEO, CardContainerC } from "@/components";
+import { PlaylistPageProps } from "@/interfaces";
+import { SEO, PlaylistCantainerCard } from "@/components";
 import { getPlaylistPageProps } from "@/utils";
 
-const PlaylistPage = ({ seoMeta, playlist }: PageProps & { playlist: any }) => {
-  const router = useRouter();
-
+const PlaylistPage = ({ playlist, seoMeta }: PlaylistPageProps) => {
   if (!playlist) return <p>No playlist found</p>;
 
   return (
     <>
       <SEO seoMeta={seoMeta} />
       <div>
-        <CardContainerC playlist={playlist} />
+        <PlaylistCantainerCard playlist={playlist} />
       </div>
     </>
   );
