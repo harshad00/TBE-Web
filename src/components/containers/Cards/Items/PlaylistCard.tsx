3c842384b95737fc69a7b9afb13416961d67defe
Harshad Prajapati <bholo0090@gmail.com>
2025-02-18 16:30:04 +0530

    Add Playlist Card

--- src/components/containers/Cards/Items/PlaylistCard.tsx
+++ src/components/containers/Cards/Items/PlaylistCard.tsx
@@ -1,50 +1,46 @@
-import { Image, Text, LinkButton } from '@/components';
-
+import React from 'react';
+import { Image, Text } from '@/components';
 import { PlaylistCardProps } from '@/interfaces';
 
 const PlaylistCard = ({
   title,
   description,
   thumbnail,
-  route,
+  playlistVideo,
+  videoId,
 }: PlaylistCardProps) => {
   return (
-    <div>
-      <div className='flex flex-col items-center w-full'>
-        <div className='w-full py-2 transition-transform duration-300 hover:scale-105'>
-          <Image
-            className='w-full h-auto md:h-[20rem] lg:h-[25rem] rounded-lg object-cover shadow-lg'
-            src={thumbnail}
-            alt={title}
-          />
+    <div className='flex justify-center'>
+      <div className='w-full p-2 md:max-w-[70%] flex flex-col gap-4'>
+        <div className='w-full border-2 border-black rounded-md overflow-hidden transition-transform duration-300 hover:scale-105'>
+          {playlistVideo ? (
+            <iframe
+              className='w-full h-56 md:h-72 lg:h-96 rounded-md'
+              src={`https://www.youtube.com/embed/${videoId}`}
+              title='YouTube Video'
+              frame-Border='0'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+              allowFullScreen
+            ></iframe>
+          ) : (
+            <Image
+              className='w-full h-auto md:h-[20rem] lg:h-[25rem] object-cover shadow-lg rounded-lg'
+              src={thumbnail}
+              alt={title}
+            />
+          )}
         </div>
-      </div>
 
-      <div className='w-full mx-auto py-2  '>
-        <Text
-          level='label'
-          className='heading-4 font-bold text-lg md:text-xl lg:text-2xl'
-        >
-          {title}
-        </Text>
-        <Text
-          level='p'
-          className='paragraph mt-1 pt-1 text-sm md:text-base lg:text-lg line-clamp-2'
-        >
-          {description}
-        </Text>
-      </div>
-      <div>
-        <div className='w-full max-w-[25rem] py-2 m-auto'>
-          <LinkButton
-            href={route || ''}
-            className='w-full sm:w-fit'
-            buttonProps={{
-              variant: 'PRIMARY',
-              text: 'Start Learning',
-              className: 'w-full ',
-            }}
-          />
+        <div className='w-full text-center'>
+          <Text level='h2' className='font-bold text-lg md:text-xl lg:text-2xl'>
+            {title}
+          </Text>
+          <Text
+            level='p'
+            className='text-sm md:text-base lg:text-lg mt-1 pt-1 line-clamp-2'
+          >
+            {description}
+          </Text>
         </div>
       </div>
     </div>
