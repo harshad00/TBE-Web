4f3cb26c9934d7829c8016238a5556ea07695fee
Harshad Prajapati <bholo0090@gmail.com>
2025-02-21 11:59:55 +0530

    Remove SEO metadata for YouFocus Add Playlist page

--- src/constant/pages/seo.ts
+++ src/constant/pages/seo.ts
@@ -246,15 +246,6 @@ const getSEOMeta = (basePath: any): GetSEOMetaResponseType => {
         'Focus,concentration, The Boring Education, Online Learning, Programming, College Students, Working professional, Career Development, Skill Enhancement, GitHub, Instagram, Twitter, LinkedIn',
       ...seoCommonMeta,
     },
-    [`${routes.youfocusAddPlaylist}`]: {
-      title: 'YouFocus-Add Playlist | The Boring Education',
-      siteName: 'Add Playlist in YouFocus at The Boring Education',
-      description: 'Add you playlist here.',
-      url: routes.youfocusAddPlaylist,
-      keywords:
-        'Focus,concentration, The Boring Education, Online Learning, Programming, College Students, Working professional, Career Development, Skill Enhancement, GitHub, Instagram, Twitter, LinkedIn',
-      ...seoCommonMeta,
-    },
     [`${routes.cohort.bringYourIdea}`]: {
       title: 'Bring Your Idea | The Boring Education',
       siteName: 'Bring Your Idea at The Boring Education',
